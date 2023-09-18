d93c1e9543dc620ee0d46e72759ad7e304138212
Renan Damascena <renandamascena@gmail.com>
2023-09-18 19:57:59 -0300

    feat: Links + Icons files

--- src/components/Perfil/containers/Links.jsx
+++ src/components/Perfil/containers/Links.jsx
@@ -1,9 +1,12 @@
+import { LinkedInIcon, InstagramIcon, WhatsAppIcon } from './Icons'
 import '../styles/Links.sass'
 
 function Links(){
     return(
         <ul className='Links'>
-            <li><a className='Link'><img src='icons/WhatsApp.svg' alt='Link__Icon' className='Link__Icon'/>celular</a></li> 
+            <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><LinkedInIcon/></a></li> 
+            <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><InstagramIcon/></a></li> 
+            <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><WhatsAppIcon/></a></li> 
         </ul>
     )
 }
