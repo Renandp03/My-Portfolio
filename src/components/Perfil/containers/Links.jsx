e4e76c49642794eb7d81e0bd667379770c774e89
Renan Damascena <renandamascena@gmail.com>
2023-09-18 20:12:11 -0300

    Feat: GitHub icon

--- src/components/Perfil/containers/Links.jsx
+++ src/components/Perfil/containers/Links.jsx
@@ -1,9 +1,10 @@
-import { LinkedInIcon, InstagramIcon, WhatsAppIcon } from './Icons'
+import { LinkedInIcon, InstagramIcon, WhatsAppIcon, GitHubIcon } from './Icons'
 import '../styles/Links.sass'
 
 function Links(){
     return(
         <ul className='Links'>
+            <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><GitHubIcon/></a></li> 
             <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><LinkedInIcon/></a></li> 
             <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><InstagramIcon/></a></li> 
             <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><WhatsAppIcon/></a></li> 
