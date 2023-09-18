1783e6f967e5e570df3a33631246c323ef23f2d3
Renan Damascena <renandamascena@gmail.com>
2023-09-18 16:59:09 -0300

    feat: Perfil links files

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,3 +1,4 @@
+import Links from './containers/Links'
 import './styles/Perfil.sass'
 
 function Perfil(){
@@ -5,7 +6,7 @@ function Perfil(){
         <div className='Perfil'>
             <img src='https://i.pinimg.com/564x/ae/bb/52/aebb524666bc7dd96bea7db24c8a3e93.jpg' alt='Perfil_img' className='Perfil__Img'/>
             <h1 className='Perfil__Title'>Renan Damascena | Desenvolvedor Full Stack</h1>
-            <ul className='Perfil__Links'></ul>
+            <Links/>
         </div>
     )
 }
---
+++ src/components/Perfil/containers/Links.jsx
@@ -0,0 +1,11 @@
+import '../styles/Links.sass'
+
+function Links(){
+    return(
+        <ul className='Links'>
+            <li><a className='Link'><img src='icons/WhatsApp.svg' alt='Link__Icon' className='Link__Icon'/>celular</a></li> 
+        </ul>
+    )
+}
+
+export default Links
