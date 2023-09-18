0fdbf3923610aeb654b4a3907aa40c959c78273e
Renan Damascena <renandamascena@gmail.com>
2023-09-18 16:08:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Perfil from "./components/Perfil/Perfil"
+
+function App() {
+
+  return (
+    <>
+    <Perfil/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Perfil/Perfil.jsx
@@ -0,0 +1,13 @@
+import './styles/Perfil.sass'
+
+function Perfil(){
+    return(
+        <div className='Perfil'>
+            <img src='https://i.pinimg.com/564x/ae/bb/52/aebb524666bc7dd96bea7db24c8a3e93.jpg' alt='Perfil_img' className='Perfil__Img'/>
+            <h1 className='Perfil__Title'>Renan Damascena | Desenvolvedor Full Stack</h1>
+            <ul className='Perfil__Links'></ul>
+        </div>
+    )
+}
+
+export default Perfil
