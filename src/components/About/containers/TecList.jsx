2bf30502cda3224a57d85345cadfdad2e3990fa2
Renan Damascena Pereira <renandamascena@gmail.com>
2023-09-21 20:58:32 -0300

    feat: react-icons

--- src/components/About/containers/TecList.jsx
+++ src/components/About/containers/TecList.jsx
@@ -1,11 +1,24 @@
+import {FaLinkedinIn, FaReact} from 'react-icons/fa'
+import '../styles/TecList.sass'
+
 function TecList(){
+
+    const tecs = [
+        {name:'React',icon:<FaReact/>, link:'https://legacy.reactjs.org/docs/getting-started.html'},
+        {name:'React',icon:<FaReact/>, link:'https://legacy.reactjs.org/docs/getting-started.html'},
+        {name:'React',icon:<FaReact/>, link:'https://legacy.reactjs.org/docs/getting-started.html'},
+    ]
+
     return(
-        <ul className='TecList'>
-            
-            <li className='Tec'>React</li>
-            <li className='Tec'>React</li>
-            <li className='Tec'>React</li>
-        </ul>
+        <div className='TecList'>
+            {tecs.map((t) => 
+                <a href={t.link} 
+                className='TecList__Icon'
+                id={t.name}
+                key={t.name}>
+                    {t.icon}
+                </a>)}
+        </div>    
     )
 }
 
