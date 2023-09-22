91e872def50cd9466390454c9f89510304abb034
Renan Damascena Pereira <renandamascena@gmail.com>
2023-09-22 14:34:14 -0300

    update: tecList + icons

--- src/components/About/containers/TecList.jsx
+++ src/components/About/containers/TecList.jsx
@@ -1,12 +1,23 @@
-import {FaLinkedinIn, FaReact} from 'react-icons/fa'
+import {FaHtml5, FaCss3, FaReact, FaNodeJs} from 'react-icons/fa'
+import { HTML5, CSS3, JavaScript, TypeScript, React, Vite, Jest, Node,MongoDB, PostgreSQL,SASS } from '../../../../public/Icons'
 import '../styles/TecList.sass'
 
 function TecList(){
 
+    const ICONS_CLASSNAME = 'TecList__Icon'
+
     const tecs = [
-        {name:'React',icon:<FaReact/>, link:'https://legacy.reactjs.org/docs/getting-started.html'},
-        {name:'React',icon:<FaReact/>, link:'https://legacy.reactjs.org/docs/getting-started.html'},
-        {name:'React',icon:<FaReact/>, link:'https://legacy.reactjs.org/docs/getting-started.html'},
+        {name:'HTML5',icon:<HTML5 className={ICONS_CLASSNAME}/>, link:'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+        {name:'CSS3',icon:<CSS3 className={ICONS_CLASSNAME}/>, link:'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+        {name:'JavaScript',icon:<JavaScript className={ICONS_CLASSNAME}/>, link:'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+        {name:'SASS',icon:<SASS className={ICONS_CLASSNAME}/>, link:'https://sass-lang.com/documentation/'},
+        {name:'TypeScript',icon:<TypeScript className={ICONS_CLASSNAME}/>, link:'https://www.typescriptlang.org/docs/'},
+        {name:'React',icon:<React className={ICONS_CLASSNAME}/>, link:'https://legacy.reactjs.org/docs/getting-started.html'},
+        {name:'Vite',icon:<Vite className={ICONS_CLASSNAME}/>, link:'https://vitejs.dev/'},
+        {name:'Node',icon:<Node className={ICONS_CLASSNAME}/>, link:'https://nodejs.org/en/docs'},
+        {name:'MongoDB',icon:<MongoDB className={ICONS_CLASSNAME}/>, link:'https://www.mongodb.com/pt-br'},
+        {name:'PostgreSQL',icon:<PostgreSQL className={ICONS_CLASSNAME}/>, link:'https://www.postgresql.org/docs/'},
+        {name:'Jest',icon:<Jest className={ICONS_CLASSNAME}/>, link:'https://jestjs.io/'},
     ]
 
     return(
--- src/components/Perfil/containers/Links.jsx
+++ src/components/Perfil/containers/Links.jsx
@@ -2,12 +2,13 @@ import { GitHubIcon, LinkedInIcon, InstagramIcon,WhatsAppIcon } from '../../../.
 import '../styles/Links.sass'
 
 function Links(){
+    const ICONS_CLASSNAME = 'Link__Icon'
     return(
         <ul className='Links'>
-            <li className='Link'><a href='https://github.com/renandp03'><GitHubIcon/></a></li> 
-            <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><LinkedInIcon/></a></li> 
-            <li className='Link'><a href='https://www.instagram.com/renan_dp/'><InstagramIcon/></a></li> 
-            <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><WhatsAppIcon/></a></li> 
+            <li className='Link'><a href='https://github.com/renandp03'><GitHubIcon className={ICONS_CLASSNAME}/></a></li> 
+            <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><LinkedInIcon className={ICONS_CLASSNAME}/></a></li> 
+            <li className='Link'><a href='https://www.instagram.com/renan_dp/'><InstagramIcon className={ICONS_CLASSNAME}/></a></li> 
+            <li className='Link'><a href='https://www.linkedin.com/in/renan-damascena/'><WhatsAppIcon className={ICONS_CLASSNAME}/></a></li> 
         </ul>
     )
 }
