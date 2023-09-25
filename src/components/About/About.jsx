e36d6ab9ae4960125fa5a3c35dbe5db14a68a77e
Renan Damascena <renandamascena@gmail.com>
2023-09-25 11:56:08 -0300

    update: About grit template

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,11 +4,14 @@ import './styles/About.sass'
 function About(){
     return(
         <div id='About'>
-            <h1 className='About__Title'>Sobre Mim<span className='About__Detail'>.</span></h1>
-            <p className='About__Text'>
-                Sou um desenvolvedor Full Stack formado pela Driven Education e atualmente procuro me desenvolver profissionalmente
-                me envolvendo em grandes projetos.
-            </p>
+            <div className='AboutMe'>
+                <h1 className='About__Title'>Sobre Mim<span className='About__Detail'>.</span></h1>
+                <p className='About__Text'>
+                    Sou um desenvolvedor Full Stack formado pela Driven Education e atualmente procuro me desenvolver profissionalmente
+                    me envolvendo em grandes projetos.
+                </p>
+            </div>
+            
             <TecList/>
         </div>
     )
--- src/components/About/containers/TecList.jsx
+++ src/components/About/containers/TecList.jsx
@@ -1,5 +1,5 @@
 import {FaHtml5, FaCss3, FaReact, FaNodeJs} from 'react-icons/fa'
-import { HTML5, CSS3, JavaScript, TypeScript, React, Vite, Jest, Node,MongoDB, PostgreSQL,SASS } from '../../../../public/Icons'
+import { HTML5, CSS3, JavaScript, TypeScript, React, Vite, Jest, Node,MongoDB, PostgreSQL,SASS, Nest } from '../../../../public/Icons'
 import '../styles/TecList.sass'
 
 function TecList(){
@@ -10,24 +10,27 @@ function TecList(){
         {name:'HTML5',icon:<HTML5 className={ICONS_CLASSNAME}/>, link:'https://developer.mozilla.org/en-US/docs/Web/HTML'},
         {name:'CSS3',icon:<CSS3 className={ICONS_CLASSNAME}/>, link:'https://developer.mozilla.org/en-US/docs/Web/CSS'},
         {name:'JavaScript',icon:<JavaScript className={ICONS_CLASSNAME}/>, link:'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
-        {name:'SASS',icon:<SASS className={ICONS_CLASSNAME}/>, link:'https://sass-lang.com/documentation/'},
         {name:'TypeScript',icon:<TypeScript className={ICONS_CLASSNAME}/>, link:'https://www.typescriptlang.org/docs/'},
         {name:'React',icon:<React className={ICONS_CLASSNAME}/>, link:'https://legacy.reactjs.org/docs/getting-started.html'},
         {name:'Vite',icon:<Vite className={ICONS_CLASSNAME}/>, link:'https://vitejs.dev/'},
+        {name:'SASS',icon:<SASS className={ICONS_CLASSNAME}/>, link:'https://sass-lang.com/documentation/'},
         {name:'Node',icon:<Node className={ICONS_CLASSNAME}/>, link:'https://nodejs.org/en/docs'},
         {name:'MongoDB',icon:<MongoDB className={ICONS_CLASSNAME}/>, link:'https://www.mongodb.com/pt-br'},
         {name:'PostgreSQL',icon:<PostgreSQL className={ICONS_CLASSNAME}/>, link:'https://www.postgresql.org/docs/'},
         {name:'Jest',icon:<Jest className={ICONS_CLASSNAME}/>, link:'https://jestjs.io/'},
+        {name:'Nest',icon:<Nest className={ICONS_CLASSNAME}/>, link:'https://docs.nestjs.com/'},
     ]
 
     return(
         <div className='TecList'>
+            <h1 className='TecList__Title'>Minhas tecnologias</h1>
             {tecs.map((t) => 
                 <a href={t.link} 
-                className='TecList__Icon'
+                className='TecList__Link'
                 id={t.name}
                 key={t.name}>
                     {t.icon}
+                    {t.name}
                 </a>)}
         </div>    
     )
